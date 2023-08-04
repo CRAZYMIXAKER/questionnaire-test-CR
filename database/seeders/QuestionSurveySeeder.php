3cdba445ad4c9e68b41bc96687b9ced3b11e83ea
Mikhail Makavetski <zombisuki@gmail.com>
2023-08-04 16:22:00 +0200

    update database logic

---
+++ database/seeders/QuestionSurveySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Question;
+use App\Models\QuestionSurvey;
+use App\Models\Survey;
+use Illuminate\Database\Seeder;
+
+class QuestionSurveySeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach (Survey::all() as $survey) {
+            for ($i = 0; $i < 3; $i++) {
+                QuestionSurvey::create([
+                    'question_id' => Question::inRandomOrder()->value('id'),
+                    'survey_id' => $survey->id,
+                ]);
+            }
+        }
+    }
+
+}
